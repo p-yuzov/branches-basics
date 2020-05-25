1bcdbdbe673e5e70d07cd2104f7c48ab6b0ef3f1
p-yuzov <p.yuzov@gmail.com>
2020-05-25 18:54:27 +0700

    add input schools

--- main.c
+++ main.c
@@ -13,5 +13,11 @@ struct school
 school citySchools[SCHOOL_COUNT];
 int main()
 {
+    for (int i = 0; i < SCHOOL_COUNT; i++) {
+        printf("\n введите номер школы, кол-вол выпускников, кол-во поступивших:\n");
+        scanf("%i%i%i", &citySchools[i].Number, &citySchools[i].Leaver, &citySchools[i].Student);
+        citySchools[i].PercentStudent = (double) 100 * citySchools[i].Student
+                                        / citySchools[i].Leaver;
+    }
     return 0;
 }
