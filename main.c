bffa631eabd02636229f6e40d9445e1293f6e16b
p-yuzov <p.yuzov@gmail.com>
2020-05-25 18:52:51 +0700

    add struct school

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <conio.h>
+#include <cstdlib>
+#include <stdio.h>
+#include <string.h>
+#define SCHOOL_COUNT 5
+struct school
+{
+    int Number;
+    int Leaver;
+    int Student;
+    double PercentStudent;
+};
+school citySchools[SCHOOL_COUNT];
+int main()
+{
+    return 0;
+}
